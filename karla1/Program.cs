18297fe2eb8a29a164d586e04b1aa7e5b07020ef
Gašper Govekar <govekar.gasper@gmail.com>
2022-12-05 14:48:52 +0100

    karla1

---
+++ karla1/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.IO.File;
+
+var rounds = ReadAllLines("./karla1/karla.txt");
+long sum = 0;
+foreach(var round in rounds){
+    var re = round[0];
+    var sh = (short)round[2];
+    var tard = (char)(sh-23);
+    sh -= 87;
+    sum += sh;
+    if(re == tard){
+        sum += 3;
+        continue;
+    }
+    if(tard == 'A' && re == 'C' || tard == 'B' && re == 'A' || tard == 'C' && re == 'B'){
+        sum += 6;
+    }
+}
+Console.WriteLine($"{sum}");
