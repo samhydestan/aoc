61431f67b2805c116730bc59a3d9d1935c97ebf4
Gašper Govekar <govekar.gasper@gmail.com>
2022-12-06 00:01:57 +0100

    patricije

---
+++ patricija1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.IO.File;
+
+var pairs = ReadAllLines("./patricija1/patricija.txt");
+long c = 0;
+foreach(var pair in pairs){
+    var elves = pair.Split(',');
+    var f = elves[0].Split('-');
+    var s = elves[1].Split('-');
+    var limits = new long[4]{
+        long.Parse(f[0]),
+        long.Parse(f[1]),
+        long.Parse(s[0]),
+        long.Parse(s[1])
+    };
+    if(limits[0]<=limits[2] && limits[1]>=limits[3] ||
+        limits[0]>=limits[2] && limits[1]<=limits[3]){
+        c++;
+    }
+}
+Console.WriteLine($"{c}");
