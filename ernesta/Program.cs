277f596784723b6c557312d78aab24f9e4572768
Gašper Govekar <govekar.gasper@gmail.com>
2022-12-05 13:22:32 +0100

    ernesta

---
+++ ernesta/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.IO.File;
+
+string[] food = ReadAllLines("./ernesta/ernesta.txt");
+long b1g = 0;
+long carry = 0;
+foreach(var item in food){
+    if(item.Length>0){
+        carry += long.Parse(item);
+    } else{
+        b1g = carry>b1g ? carry : b1g;
+        carry = 0;
+    }
+}
+b1g = carry>b1g ? carry : b1g;
+Console.WriteLine($"{b1g}");
