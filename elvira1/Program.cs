8c6c48edc044a77b347bd72e21ee31e07da942a5
Gašper Govekar <govekar.gasper@gmail.com>
2022-12-06 21:02:47 +0100

    elviri

---
+++ elvira1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.IO.File;
+
+var txt = ReadAllText("./elvira1/elvira.txt");
+int p = 0;
+var q = new Queue<char>(5);
+while(p<txt.Length){
+    q.Enqueue(txt[p]);
+    if(q.Count == 5){
+        q.Dequeue();
+        if(q.Distinct().Count() == q.Count){
+            break;
+        }
+    }
+    p++;
+}
+Console.WriteLine(p+1);
