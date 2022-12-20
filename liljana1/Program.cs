7e9db9879ac6fdb0e26a20481f275e4a81a78678
Gašper Govekar <govekar.gasper@gmail.com>
2022-12-20 16:50:07 +0100

    .

--- liljana1/Program.cs
+++ liljana1/Program.cs
@@ -4,15 +4,23 @@ using System.Diagnostics;
 var stop = new Stopwatch();
 stop.Start();
 var lines = ReadAllLines("./liljana1/liljana.txt");
-var cubes = new List<int[]>();
+var cubes = new HashSet<(int, int, int)>();
 int sides = 0;
 foreach(var line in lines){
     var cube = line.Split(',').Select(x => int.Parse(x)).ToArray();
-    sides += 6 - 2*cubes.Where(x =>
-        x[0] == cube[0] && x[1] == cube[1] && Math.Abs(x[2]-cube[2]) == 1 ||
-        x[0] == cube[0] && x[2] == cube[2] && Math.Abs(x[1]-cube[1]) == 1 ||
-        x[1] == cube[1] && x[2] == cube[2] && Math.Abs(x[0]-cube[0]) == 1)
-        .Count();
-    cubes.Add(cube);
+    var minus = (cubes.Contains((cube[0], cube[1], cube[2]+1)) ? 2 : 0) +
+                (cubes.Contains((cube[0], cube[1], cube[2]-1)) ? 2 : 0) +
+                (cubes.Contains((cube[0], cube[1]+1, cube[2])) ? 2 : 0) +
+                (cubes.Contains((cube[0], cube[1]-1, cube[2])) ? 2 : 0) +
+                (cubes.Contains((cube[0]+1, cube[1], cube[2])) ? 2 : 0) +
+                (cubes.Contains((cube[0]-1, cube[1], cube[2])) ? 2 : 0);
+    sides += 6 - minus;
+    cubes.Add((cube[0], cube[1], cube[2]));
 }
+/*foreach(var cube in cubes){
+    if(cubes.Any(x => x[0] == cube[0] && x[1] == cube[1]+1 && x[2] == cube[2]+1) &&
+        ){
+        sides -= 6;
+    }
+}*/
 Console.WriteLine(sides);
